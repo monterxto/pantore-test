080fbd277394f528a02c1ed14030cadc77937d33
Antonio <toniko1910@gmail.com>
2024-11-27 18:29:19 -0300

    feat: lower case and nullable type utils

---
+++ src/utils/transformers/lower-case.transformer.ts
@@ -0,0 +1,5 @@
+import { TransformFnParams } from 'class-transformer/types/interfaces';
+
+export const lowerCaseTransformer = (
+  params: TransformFnParams,
+): string => params.value?.toLowerCase().trim();
