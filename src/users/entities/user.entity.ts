eaadade5a4a5a336f958f1c33c959e4729a7e75a
Antonio <toniko1910@gmail.com>
2024-11-27 18:26:09 -0300

    feat: user entity swagger

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,9 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { now, HydratedDocument } from 'mongoose';
+import { now, HydratedDocument, Types } from 'mongoose';
 
-import { Expose } from 'class-transformer';
+import { Exclude, Expose, Type } from 'class-transformer';
+import { Role } from '../../roles/entities/role.entity';
+import { ApiResponseProperty } from '@nestjs/swagger';
 
 @Schema({
   timestamps: true,
@@ -11,6 +13,10 @@ import { Expose } from 'class-transformer';
   },
 })
 export class User {
+  @ApiResponseProperty({
+    type: String,
+    example: 'john.doe@example.com',
+  })
   @Prop({
     type: String,
     unique: true,
@@ -18,25 +24,41 @@ export class User {
   @Expose({ groups: ['me', 'admin'], toPlainOnly: true })
   email: string | null;
 
+  @ApiResponseProperty({
+    type: String,
+    example: 'John',
+  })
   @Prop({
     type: String,
   })
   name: string | null;
 
-  @Prop({
-    type: String,
+  @ApiResponseProperty({
+    type: () => Role,
   })
-  role?: string | null;
+  @Prop({
+    type: Role,
+  })
+  role?: Role | null;
 
-  @Prop({ default: now })
-  createdAt: Date;
-
-  @Prop({ default: now })
-  updatedAt: Date;
-
+  @Exclude({ toPlainOnly: true })
   @Prop()
-  deletedAt: Date;
+  password?: string;
+
+  @ApiResponseProperty()
+  @Prop({ default: now })
+  createdAt?: Date;
+
+  @ApiResponseProperty()
+  @Prop({ default: now })
+  updatedAt?: Date;
+
+  @ApiResponseProperty()
+  @Prop()
+  deletedAt?: Date;
 }
 
 export type UserSchemaDocument = HydratedDocument<User>;
 export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.index({ 'role.id': 1 });
