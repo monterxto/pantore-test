6dbd218a119388c559e54492e0ac8626141187f3
Antonio <toniko1910@gmail.com>
2024-11-29 03:36:51 -0300

    feat: user create dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { Transform, Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
+import { RoleDto } from '../../roles/dto/role.dto';
+import { lowerCaseTransformer } from '../../utils/transformers/lower-case.transformer';
+
+export class CreateUserDto {
+  @ApiProperty({ example: 'test1@example.com', type: String })
+  @Transform(lowerCaseTransformer)
+  @IsNotEmpty()
+  @IsEmail()
+  email: string | null;
+
+  @ApiProperty({ example: 'John', type: String })
+  @IsNotEmpty()
+  name: string | null;
+  
+  @ApiProperty()
+  @MinLength(6)
+  password?: string;
+
+  @ApiPropertyOptional({ type: RoleDto })
+  @IsOptional()
+  @Type(() => RoleDto)
+  role?: RoleDto | null;
+}
