ecc4fdaf182b2c23a095ee4341d9bbbed266b3bb
Antonio <toniko1910@gmail.com>
2024-11-29 03:36:43 -0300

    feat: user create service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersRepository } from './repositories/user.repository';
+import { RoleEnum } from 'src/roles/roles.enum';
+import * as bcrypt from 'bcrypt';
+import { NullableType } from 'src/utils/types/nullable.type';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    private readonly usersRepository: UsersRepository
+  ) {}
+  async create(createUserDto: CreateUserDto):  Promise<User> {
+    if (createUserDto.email) {
+      const userObject = await this.usersRepository.findByEmail(
+        createUserDto.email,
+      );
+      if (userObject) {
+        throw new UnprocessableEntityException({
+          status: HttpStatus.UNPROCESSABLE_ENTITY,
+          errors: {
+            email: 'emailAlreadyExists',
+          },
+        });
+      }
+    }
+
+    if (createUserDto.password) {
+      const salt = await bcrypt.genSalt();
+      createUserDto.password = await bcrypt.hash(createUserDto.password, salt);
+    }
+
+    if (createUserDto.role?.id) {
+      const roleObject = Object.values(RoleEnum)
+        .map(String)
+        .includes(String(createUserDto.role.id));
+      if (!roleObject) {
+        throw new UnprocessableEntityException({
+          status: HttpStatus.UNPROCESSABLE_ENTITY,
+          errors: {
+            role: 'roleNotExists',
+          },
+        });
+      }
+    }
+
+    return this.usersRepository.create(createUserDto);
+  }
+  
+  findById(id: string): Promise<NullableType<User>> {
+    return this.usersRepository.findById(id);
+  }
+
+  findByEmail(email: string): Promise<NullableType<User>> {
+    return this.usersRepository.findByEmail(email);
   }
 
   findAll() {
