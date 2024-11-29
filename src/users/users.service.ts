3b12a7f3b6c1f20404ecaab992121cc5d260d2c9
Antonio <toniko1910@gmail.com>
2024-11-29 17:55:57 -0300

    feat: update method in service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,13 +6,14 @@ import { RoleEnum } from 'src/roles/roles.enum';
 import * as bcrypt from 'bcrypt';
 import { NullableType } from 'src/utils/types/nullable.type';
 import { User } from './entities/user.entity';
+import { JwtPayloadType } from 'src/utils/types/jwt-payload.type';
 
 @Injectable()
 export class UsersService {
   constructor(
     private readonly usersRepository: UsersRepository
-  ) {}
-  async create(createUserDto: CreateUserDto):  Promise<User> {
+  ) { }
+  async create(createUserDto: CreateUserDto): Promise<User> {
     if (createUserDto.email) {
       const userObject = await this.usersRepository.findByEmail(
         createUserDto.email,
@@ -48,7 +49,7 @@ export class UsersService {
 
     return this.usersRepository.create(createUserDto);
   }
-  
+
   findById(id: string): Promise<NullableType<User>> {
     return this.usersRepository.findById(id);
   }
@@ -65,8 +66,54 @@ export class UsersService {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(
+    id: User['_id'],
+    payload: UpdateUserDto,
+  ): Promise<User | null> {
+
+    if (payload.password) {
+      const salt = await bcrypt.genSalt();
+      payload.password = await bcrypt.hash(payload.password, salt);
+    }
+
+    if (payload.email) {
+      const userObject = await this.usersRepository.findByEmail(
+        payload.email,
+      );
+
+      if (userObject && userObject._id !== id) {
+        throw new UnprocessableEntityException({
+          status: HttpStatus.UNPROCESSABLE_ENTITY,
+          errors: {
+            email: 'emailAlreadyExists',
+          },
+        });
+      }
+    }
+
+    if (payload.role?.id) {
+      const roleObject = Object.values(RoleEnum)
+        .map(String)
+        .includes(String(payload.role.id));
+      if (!roleObject) {
+        throw new UnprocessableEntityException({
+          status: HttpStatus.UNPROCESSABLE_ENTITY,
+          errors: {
+            role: 'roleNotExists',
+          },
+        });
+      }
+      payload.role.name = RoleEnum[payload.role.id]
+    }
+
+    return this.usersRepository.update(id, payload);
+  }
+
+  async updateMe(
+    userJwtPayload: JwtPayloadType,
+    payload: UpdateUserDto,
+  ): Promise<any> {
+    return this.update(userJwtPayload._id, payload)
   }
 
   remove(id: number) {
