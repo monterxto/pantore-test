dbde8c08a8b33a0cbe39cb103f6854f962312360
Antonio <toniko1910@gmail.com>
2024-11-29 03:45:15 -0300

    feat: auth variables envs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConfigService } from './config/database/mongoose-config.service';
+import { AuthModule } from './auth/auth.module';
 import databaseConfig from './config/database/database.config';
 import appConfig from './config/main/app.config';
+import authConfig from './config/auth/auth.config';
 
 const infrastructureDatabaseModule = MongooseModule.forRootAsync({
   useClass: MongooseConfigService,
@@ -17,11 +19,13 @@ const infrastructureDatabaseModule = MongooseModule.forRootAsync({
       isGlobal: true,
       load: [
         databaseConfig,
+        authConfig,
         appConfig
       ],
       envFilePath: ['.env'],
     }),
-    infrastructureDatabaseModule
+    infrastructureDatabaseModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
---
+++ src/config/auth/auth.config.ts
@@ -0,0 +1,22 @@
+import { registerAs } from '@nestjs/config';
+
+import { IsString } from 'class-validator';
+import validateConfig from '../../utils/validate-config';
+import { AuthConfig } from './auth-config.type';
+
+class EnvironmentVariablesValidator {
+  @IsString()
+  AUTH_JWT_SECRET: string;
+
+  @IsString()
+  AUTH_JWT_TOKEN_EXPIRES_IN_MINUTES: string;
+}
+
+export default registerAs<AuthConfig>('auth', () => {
+  validateConfig(process.env, EnvironmentVariablesValidator);
+
+  return {
+    secret: process.env.AUTH_JWT_SECRET,
+    expires: process.env.AUTH_JWT_TOKEN_EXPIRES_IN_MINUTES,
+  };
+});
