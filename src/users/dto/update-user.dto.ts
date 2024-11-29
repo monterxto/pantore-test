1b68df68721b8a35b107b3eea5cc6e4239abbc50
Antonio <toniko1910@gmail.com>
2024-11-29 17:58:21 -0300

    feat: update user dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,30 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, ApiPropertyOptional } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+import { Transform, Type } from 'class-transformer';
+import { IsEmail, IsOptional, MinLength, ValidateNested } from 'class-validator';
+import { RoleDto } from '../../roles/dto/role.dto';
+import { lowerCaseTransformer } from '../../utils/transformers/lower-case.transformer';
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiPropertyOptional({ example: 'test1@example.com', type: String })
+  @Transform(lowerCaseTransformer)
+  @IsOptional()
+  @IsEmail()
+  email?: string | null;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @MinLength(6)
+  password?: string;
+
+  @ApiPropertyOptional({ example: 'John', type: String })
+  @IsOptional()
+  name?: string | null;
+
+  @ApiPropertyOptional({ type: () => RoleDto })
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => RoleDto)
+  role?: RoleDto | null;
+}
